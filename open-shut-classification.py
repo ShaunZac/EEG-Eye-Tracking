c4923060cebe2a172f6884ca13be3494efed3cd2
Shaun Zacharia <shaunzacharia5@gmail.com>
2020-11-30 19:36:09 +0530

    Initial setup

---
+++ open-shut-classification.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+df = pd.read_csv("eeg-eye-state.csv")
+# perform bandpass filtering on input
+
+# extract min and max amplitude from temporal windows
+
+# Perform ICA on the min-max channels
+
+# Use an ML algorithm to predict
