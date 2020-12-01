15617b91df022abdc923ddf74ee416dd386aa695
Shaun Zacharia <shaunzacharia5@gmail.com>
2020-12-01 23:21:01 +0530

    Added bpf function

--- open-shut-classification.py
+++ open-shut-classification.py
@@ -1,8 +1,18 @@
+import numpy as np
 import pandas as pd
+from scipy.signal import butter, lfilter
+
+
+def bpf(data: pd.DataFrame) -> pd.DataFrame:
+    b, a = butter(10, [1, 40], 'band', False, 'ba', 128)
+    for i in range(14):
+        data.iloc[:, i] = lfilter(b, a, data.iloc[:, i])
+    return data
+
 
 df = pd.read_csv("eeg-eye-state.csv")
 # perform bandpass filtering on input
-
+df = bpf(df)
 # extract min and max amplitude from temporal windows
 
 # Perform ICA on the min-max channels
